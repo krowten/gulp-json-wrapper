99ee70755cc2bc407d956ee063f5279a7bafa19e
krowten <delphiprogrammer@inbox.ru>
2014-10-08 20:34:19 +0400

    1.0.0

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
 gulp-json-wrapper
 =================
 
-Create javascript config from json file
+Create javascript config from json file. Plugin for [gulp](https://github.com/gulpjs/gulp)
+
+#Install
+
+```
+npm install gulp-json-wrapper
+```
+
+#Basic Usage
+
+Something like this:
+
+```javascript
+var gulp = require('gulp');
+var concat = require('gulp-concat')
+var json = require('gulp-json-wrapper')
+
+gulp.task('js', function () {
+	gulp.src('./scripts/*.js')
+	    .pipe(json({
+	      src: 'path/to/config.json',
+	      namespace: 'myconfig'
+	    }))
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('./js'));
+});
+```
+
+
+## gulp-json-wrapper specific options
+
+#### `src: String`
+
+Patch to json file.
+
+#### `angular: Boolean`
+
+Wrapped in angular module. Default false.
+
+#### `module: String`
+
+Name of angular module. Default 'json'.
+
+#### `namespace: String`
+
+Variable name. Default 'config'.
---
+++ index.js
@@ -0,0 +1,67 @@
+var through = require('through2');
+var gutil = require('gulp-util');
+var fs = require('fs');
+var PluginError = gutil.PluginError;
+
+const PLUGIN_NAME = 'gulp-json-wrapper';
+
+function configStream(config) {
+  var stream = through();
+  stream.write(config);
+  return stream;
+}
+
+function gulpJsonWrapper(params) {
+
+  if (!params) {
+    throw new PluginError(PLUGIN_NAME, 'Missing params!');
+  }
+
+  if (!params.src) {
+    throw new PluginError(PLUGIN_NAME, 'Missing json file!');
+  }
+
+  if (!params.namespace) {
+    params.namespace = 'config';
+  }
+
+  var stream = through.obj(
+    function(file, enc, cb) {
+      var self = this;
+      fs.readFile(params.src, "utf8", function (err, jsonFile) {
+        if (err) throw new PluginError(PLUGIN_NAME, 'Json file not found!');
+
+        var obj = JSON.parse(jsonFile);
+
+        if (params.angular){
+          var moduleName = params.module || 'json';
+          jsonFile = 'angular.module(\''+moduleName+'\', []).value(\''+params.namespace+'\', '+JSON.stringify(obj)+');';
+        } else {
+          jsonFile = 'var ' + params.namespace + '=' + JSON.stringify(obj) + ';';
+        }
+
+        var config = new Buffer(jsonFile);
+
+        if (file.isNull()) {
+          // do nothing if no contents
+        }
+
+        if (file.isBuffer()) {
+          file.contents = Buffer.concat([file.contents, config]);
+        }
+
+        if (file.isStream()) {
+          file.contents = file.contents.pipe(configStream(jsonFile));
+        }
+
+        self.push(file);
+
+        return cb();
+      });
+    }
+  );
+
+  return stream;
+};
+
+module.exports = gulpJsonWrapper;
